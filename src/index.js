151d3e852c53cf3269efd8ce87ecdea36b626ecc
Alexandr Kondratenko <alexandr.kondratenko.exp@gmail.com>
2021-10-05 08:53:41 +0300

    залил стили и fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './sass/main.scss';
+
+import countryCardTpl from './handlebars/country-card.hbs';
+
+const cardContainer = document.querySelector('.js-card-container');
+const inputSearch = document.querySelector('.js-input-search');
+
+inputSearch.addEventListener('input', onSearch);
+
+function onSearch(event) {
+    fetchCountry(event.target.value).then(renderCountryCard);
+}
+
+function fetchCountry(params) {
+    return fetch(`https://restcountries.com/v2/name/${params}`).then(res => res.json());
+}
+
+function renderCountryCard(country) {
+    cardContainer.insertAdjacentHTML('beforeend', countryCardTpl(country));
+}
