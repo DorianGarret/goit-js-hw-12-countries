5e7b823e383bf1704bdf4c8303bc7e33105af982
Alexandr Kondratenko <alexandr.kondratenko.exp@gmail.com>
2021-10-06 13:54:55 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { debounce } from 'lodash';
 import { refs } from './js/refs';
 import CountryApiService from './js/api/countryApiService';
 import countryCardTpl from './handlebars/country-card.hbs';
+import countryListTpl from './handlebars/country-list.hbs';
 
 const countryApiService = new CountryApiService();
 
@@ -17,7 +18,8 @@ function onSearch(event) {
 }
 
 function renderCountryCard(country) {
-  refs.cardContainer.insertAdjacentHTML('beforeend', countryCardTpl(country));
+  // refs.cardContainer.insertAdjacentHTML('beforeend', countryCardTpl(country));
+  refs.cardContainer.insertAdjacentHTML('beforeend', countryListTpl(country));
 }
 
 refs.inputSearch.addEventListener('input', debounce(onSearch, 500));
